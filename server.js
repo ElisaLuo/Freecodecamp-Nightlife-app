facaf75d630655619c16914cf87283473ee92510
Elisa L <luolixuan2003@gmail.com>
2017-07-30 15:54:10 +0000

    Slight changes to server.js

--- server.js
+++ server.js
@@ -19,16 +19,14 @@ db.once('open', () => {
   console.log('Connection to the database successful');
 });
 
+app.use(express.static('public'));
 app.use(session({
     secret: 'elktrjosd0983jlwsf08easd90',
     resave: true,
     saveUninitialized: true,
 }));
-
 app.use(bodyParser.urlencoded({ extended: true }));// for parsing application/x-www-form-urlencoded
 app.use(passport.initialize());//Sets up for passport
-app.use(passport.session());
-app.use(express.static(__dirname + '/public'));
 app.set('port', process.env.PORT || process.env.IP );
 app.set('view engine', 'ejs');
 
